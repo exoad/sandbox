b929ffedcd2f6ed71eb5d3bf5697e3d72631494a
jack meng <jackmeng0814@gmail.com>
2024-11-18 17:29:01 +0000

    fix formatting for template code

--- Solves/modules/googer.cxx
+++ Solves/modules/googer.cxx
@@ -110,8 +110,8 @@ namespace judge
 using namespace judge;
 namespace generics 
 {
-    #include <ext/pb_ds/assoc_container.hpp>
-    #include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
     using namespace __gnu_pbds;
     template <typename T> 
     using ordered_set=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
