d6d054b99f4ecaa707e13ecb4af19e9b56390b0f
jackm <jackmeng0814@gmail.com>
2024-12-06 10:32:53 -0500

    Update temp.cxx

--- Solves/modules/temp.cxx
+++ Solves/modules/temp.cxx
@@ -1,48 +1,6 @@
-#include <iostream>
-#include <string.h>
-#include <vector>
-#pragma warning(disable : 4996)
-#pragma warning(disable : 4244)
-#pragma warning(disable : 4305)
-#pragma warning(disable : 4267)
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#pragma GCC diagnostic ignored "-Wpragmas"
-#pragma GCC diagnostic ignored "-Wunused-function"
-#pragma GCC diagnostic ignored "-Wunused-but-set-variable"
-#pragma GCC diagnostic ignored "-Wunused-but-set-parameter"
-#pragma GCC diagnostic ignored "-Wwrite-strings"
-#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
-#pragma GCC diagnostic ignored "-Wunused-but-set-label"
-#pragma GCC diagnostic ignored "-Wunused-label"
-#pragma CYG_ANNOT_CALL_CONVENTION
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-#pragma GCC diagnostic ignored "-Wmissing-braces"
-#pragma GCC diagnostic ignored "-Wmissing-declarations"
-using namespace std;
-// using namespace __gnu_pbds;
-#define I64 long long
-#define I32 int
-#define sz(x) I32((x).size())
-#define bg(x) begin(x)
-#define all(x) bg(x), end(x)
-#define sall(x) x.rbegin(), x.rend() 
-#define st(x) sort(all(x)) 
-#define U0 void
-#define STR string
-U0 setIO(STR name="") 
-{
-	cin.tie(0)->sync_with_stdio(0);
-	if (sz(name)) 
-    {
-		freopen((name+".in").c_str(),"r",stdin); 
-		freopen((name+".out").c_str(),"w",stdout);
-	}
-}
-I32 main()
-{
-    setIO();
-
-
-    return 0;
-}
+constexpr inline uint leq_pow2(const uint x) {return 1u << __lg(x);}
+constexpr inline ull leq_pow2ll(const ull x) {return 1ull << __lg(x);}
+constexpr inline uint geq_pow2(const uint x) {return x & (x - 1) ? 2u << __lg(x) : x;}
+constexpr inline ull geq_pow2ll(const ull x) {return x & (x - 1) ? 2ull << __lg(x) : x;}
+constexpr inline ll sqd(const pll p1, const pll p2) {return (p1.F - p2.F) * (p1.F - p2.F) + (p1.S - p2.S) * (p1.S - p2.S);}
+constexpr inline ll sqd(const ll x1, const ll y1, const ll x2, const ll y2) {return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);}
